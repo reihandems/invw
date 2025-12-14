a946ca13e50128f39d46814cc185da82eaae33a2
rdemstion <reihandchallada@gmail.com>
2025-12-14 17:55:43 +0700

    Memperbaiki kesalahan penulisan di UserModel.php

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use CodeIgniter\Model;
 
 class UserModel extends Model {
-    protected $tabel = 'user';
+    protected $table = 'user';
     protected $primaryKey = 'user_id';
     protected $allowedFields = ['username', 'nama_lengkap', 'email', 'password', 'role'];
 }
