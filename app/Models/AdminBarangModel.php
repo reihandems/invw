e7ff8e397a89f51eaf2fe0e11f8baa304067466c
rdemstion <reihandchallada@gmail.com>
2025-12-15 00:03:41 +0700

    Mengubah nama BarangModel menjadi AdminBarangModel.php

--- app/Models/AdminBarangModel.php
+++ app/Models/AdminBarangModel.php
@@ -4,9 +4,9 @@ namespace App\Models;
 
 use CodeIgniter\Model;
 
-class BarangModel extends Model {
+class AdminBarangModel extends Model {
     protected $table = 'barang';
-    protected $primaryKey = 'id';
+    protected $primaryKey = 'barang_id';
     protected $allowedFields = ['namaBarang', 'hargaBarang'];
     protected $returnType = 'array';
     protected $useTimestamps = false;
